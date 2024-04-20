75a46f724afbafb4c5dcc77ba99d79d393ccc500
mingming <jimin23623@gmail.com>
2024-04-20 22:13:35 +0900

    design: theme color 지정
    - 지금 나와있는 color까지 theme 지정했습니다.
    
    closes #18

--- src/shared/ui/styles/theme/theme.ts
+++ src/shared/ui/styles/theme/theme.ts
@@ -1,7 +1,15 @@
 import { zIndex } from '../z-index';
 
 export const theme = {
-  color: {},
+  color: {
+    black_000000: '#000000',
+    white_FFFFFF: '#FFFFFF',
+
+    yellow_FFB904: '#FFB904',
+    red_F5511D: '#F5511D',
+    red_FFC2AF: '#FFC2AF',
+    blue_2087D6: '#2087D6',
+  },
   zIndex,
 } as const;
 
