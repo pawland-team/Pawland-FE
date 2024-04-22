8b41a659188efc16648beb9ea5e3d3c10f7784bf
mingming <jimin23623@gmail.com>
2024-04-22 13:03:40 +0900

    chore: Pretendard 폰트 설정/theme 추가

--- src/shared/ui/styles/theme/theme.ts
+++ src/shared/ui/styles/theme/theme.ts
@@ -1,6 +1,19 @@
 import { zIndex } from '../z-index';
 
 export const theme = {
+  deviceSize: {
+    mobile: 'screen and (max-width: 768px)',
+    tablet: 'screen and (max-width: 1025px)',
+  },
+  font: {
+    pretendard_100: `'Pretendard-100', sans-serif`,
+    pretendard_200: `'Pretendard-200', sans-serif`,
+    pretendard_300: `'Pretendard-300', sans-serif`,
+    pretendard_400: `'Pretendard-400', sans-serif`,
+    pretendard_500: `'Pretendard-500', sans-serif`,
+    pretendard_600: `'Pretendard-600', sans-serif`,
+    pretendard_700: `'Pretendard-700', sans-serif`,
+  },
   color: {
     black_000000: '#000000',
     white_FFFFFF: '#FFFFFF',
