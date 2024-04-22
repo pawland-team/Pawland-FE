fd4f87ae35f21a6432332d38df3661b49d3abded
mingming <jimin23623@gmail.com>
2024-04-22 22:06:35 +0900

    feat: formatPrice라는 천단위로 콤마 찍는 함수 생성

--- src/entities/product/product-card-item/discription.tsx
+++ src/entities/product/product-card-item/discription.tsx
@@ -1,10 +1,12 @@
+import { formatPrice } from '@shared/utils/format-price';
+
 import * as S from './product-card-item-style';
 
 const Discription = () => {
   return (
     <S.ProductDescription>
       <h4>[직거래희망] 저희 아버지가 직접 키운 고양이가 환장하는 캣닢 유기농 쿨거 가능 </h4>
-      <p className='price'>5000</p>
+      <p className='price'>{formatPrice(5000)}</p>
       <span className='view'>조회수 20</span>
     </S.ProductDescription>
   );
