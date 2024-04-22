ba0adf1fa9ca2f8937b45a2adfd61f1dfc267400
mingming <jimin23623@gmail.com>
2024-04-22 15:11:19 +0900

    chore: cdn 방식 지우고 저장된 폰트 파일 불러오도록 수정

--- src/pages/home/home-page-style.ts
+++ src/pages/home/home-page-style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const HomePage = styled.div`
-  font-family: ${({ theme }) => theme.font.imcre};
+  font-family: ${({ theme }) => theme.font.pretendard_100};
   font-size: 5.2rem;
   color: ${({ theme }) => theme.color.gray_BDBDBD};
 `;
