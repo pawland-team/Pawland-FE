9b3719d4ccd25185c8f3280ba71eecf72503dda0
clyde <clydehan0401@gmail.com>
2024-04-24 02:58:22 +0900

    design: 여백 스타일링

--- src/pages/signup/signup-page-style.ts
+++ src/pages/signup/signup-page-style.ts
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
+const SignupPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  width: 380px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const SignupPageTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+  justify-content: center;
+
+  margin-bottom: 40px;
+`;
+
+const SignupPageForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SignupPageBottomContainer = styled.div`
+  display: flex;
+  gap: 25px;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 20px;
+`;
+
 const SignupPageTitle = styled.h1`
   font-family: Imcre-Soojin, sans-serif;
   font-size: 4rem;
@@ -28,7 +63,7 @@ const SignupPageInput = styled.input`
   font-weight: 400;
   color: ${({ theme }) => theme.color.gray_BDBDBD};
 
-  background-color: ${({ theme }) => theme.color.gray_F3F3F3};
+  background-color: ${({ theme }) => theme.color.gray_F9F9F9};
   border: 1px solid ${({ theme }) => theme.color.gray_BDBDBD};
   border-radius: 6px;
 
@@ -49,6 +84,7 @@ const SignupInputIcon = styled.img`
 const SignupPageDisabledButton = styled.button`
   width: 100%;
   height: 64px;
+  margin-top: 20px;
 
   font-family: Pretendard, sans-serif;
   font-size: 2rem;
@@ -56,6 +92,7 @@ const SignupPageDisabledButton = styled.button`
   color: ${({ theme }) => theme.color.white_FFFFFF};
 
   background-color: ${({ theme }) => theme.color.gray_9E9E9E};
+  border-radius: 6px;
 `;
 
 const SignupPageBottomSpan = styled.span`
@@ -82,4 +119,8 @@ export {
   SignupPageDisabledButton,
   SignupPageBottomSpan,
   SignupPageBottomLink,
+  SignupPageContainer,
+  SignupPageTitleContainer,
+  SignupPageForm,
+  SignupPageBottomContainer,
 };
--- src/pages/signup/signup-page.tsx
+++ src/pages/signup/signup-page.tsx
@@ -12,31 +12,35 @@ export const SignupPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <div>
-          <S.SignupPageTitle>회원가입</S.SignupPageTitle>
-          <S.SignupPageTitleSpan>회원가입에 필요한 정보를 입력해주세요.</S.SignupPageTitleSpan>
-        </div>
-        <form>
-          <S.SignupPageInputContainer>
-            <S.SignupInputIcon src='input-nickname-icon.svg' alt='input-nickname-icon' />
-            <S.SignupPageInput placeholder='닉네임을 정해주세요.' />
-          </S.SignupPageInputContainer>
-          <S.SignupPageInputContainer>
-            <S.SignupInputIcon src='input-email-icon.svg' alt='input-email-icon' />
-            <S.SignupPageInput placeholder='이메일을 적어주세요.' />
-          </S.SignupPageInputContainer>
-          <S.SignupPageInputContainer>
-            <S.SignupInputIcon src='input-password-icon.svg' alt='input-password-icon' />
-            <S.SignupPageInput placeholder='비밀번호를 적어주세요.' />
-          </S.SignupPageInputContainer>
-          <S.SignupPageInputContainer>
-            <S.SignupInputIcon src='input-password-icon.svg' alt='input-password-icon' />
-            <S.SignupPageInput placeholder='비밀번호를 한 번 더 적어주세요.' />
-          </S.SignupPageInputContainer>
-          <S.SignupPageDisabledButton type='submit'>인증을 진행해 주세요.</S.SignupPageDisabledButton>
-          <S.SignupPageBottomSpan>이미 포랜드 계정이 있으신가요?</S.SignupPageBottomSpan>
-          <S.SignupPageBottomLink>로그인하기</S.SignupPageBottomLink>
-        </form>
+        <S.SignupPageContainer>
+          <S.SignupPageTitleContainer>
+            <S.SignupPageTitle>회원가입</S.SignupPageTitle>
+            <S.SignupPageTitleSpan>회원가입에 필요한 정보를 입력해주세요.</S.SignupPageTitleSpan>
+          </S.SignupPageTitleContainer>
+          <S.SignupPageForm>
+            <S.SignupPageInputContainer>
+              <S.SignupInputIcon src='input-nickname-icon.svg' alt='input-nickname-icon' />
+              <S.SignupPageInput placeholder='닉네임을 정해주세요.' />
+            </S.SignupPageInputContainer>
+            <S.SignupPageInputContainer>
+              <S.SignupInputIcon src='input-email-icon.svg' alt='input-email-icon' />
+              <S.SignupPageInput placeholder='이메일을 적어주세요.' />
+            </S.SignupPageInputContainer>
+            <S.SignupPageInputContainer>
+              <S.SignupInputIcon src='input-password-icon.svg' alt='input-password-icon' />
+              <S.SignupPageInput placeholder='비밀번호를 적어주세요.' />
+            </S.SignupPageInputContainer>
+            <S.SignupPageInputContainer>
+              <S.SignupInputIcon src='input-password-icon.svg' alt='input-password-icon' />
+              <S.SignupPageInput placeholder='비밀번호를 한 번 더 적어주세요.' />
+            </S.SignupPageInputContainer>
+            <S.SignupPageDisabledButton type='submit'>인증을 진행해 주세요.</S.SignupPageDisabledButton>
+          </S.SignupPageForm>
+          <S.SignupPageBottomContainer>
+            <S.SignupPageBottomSpan>이미 포랜드 계정이 있으신가요?</S.SignupPageBottomSpan>
+            <S.SignupPageBottomLink>로그인하기</S.SignupPageBottomLink>
+          </S.SignupPageBottomContainer>
+        </S.SignupPageContainer>
       </main>
     </>
   );
