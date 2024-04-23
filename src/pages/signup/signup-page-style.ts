c48d4b006067c5b9cf6c82d81554173890610874
clyde <clydehan0401@gmail.com>
2024-04-24 00:43:29 +0900

    chore: 회원가입 페이지 폴더구조 기초 세팅

---
+++ src/pages/signup/signup-page-style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SignupPage = styled.div`
+  font-size: 5.2rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.gray_BDBDBD};
+`;
+
+export { SignupPage };
