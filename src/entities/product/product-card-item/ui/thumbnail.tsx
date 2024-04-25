fada731f0b605fd68d5b2fe0e052bfb445a62681
mingming <jimin23623@gmail.com>
2024-04-25 23:42:03 +0900

    refactor: image 불러올때 placeholder이미지 넣어봄

--- src/entities/product/product-card-item/ui/thumbnail.tsx
+++ src/entities/product/product-card-item/ui/thumbnail.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image';
+
 import * as S from './product-card-item-style';
 import { WishItemButton } from './whish-item-button';
 
 const Thumbnail = () => {
   return (
     <S.ProductThumbnaile className='thumbnail'>
-      <img className='thumbnail-image' src='https://loremflickr.com/600/400' alt='상품 이미지' />
+      <Image
+        width={276}
+        height={276}
+        placeholder='blur'
+        blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNcvHhTPQAGYgJ5cH4fHAAAAABJRU5ErkJggg=='
+        className='thumbnail-image'
+        src='/assets/images/product/test-image.png'
+        alt='상품 이미지'
+      />
       <WishItemButton />
     </S.ProductThumbnaile>
   );
