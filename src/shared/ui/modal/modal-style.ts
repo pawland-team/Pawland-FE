abfc4b4f7eb71d48e4046e74229e5eae408ac320
clyde <clydehan0401@gmail.com>
2024-04-26 03:55:53 +0900

    design: 모달 스타일링

---
+++ src/shared/ui/modal/modal-style.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: rgb(0 0 0 / 50%);
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 340px;
+  height: 180px;
+  padding: 20px;
+
+  background-color: ${({ theme }) => theme.color.white_FFFFFF};
+  border-radius: 10px;
+`;
+
+const ModalContentStyle = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 118px;
+
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 1.6rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.black_000000};
+`;
+
+const ModalButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 62px;
+  padding-top: 20px;
+
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+
+  border-top: 1px solid ${({ theme }) => theme.color.gray_BDBDBD};
+`;
+
+export { ModalContainer, ModalContent, ModalContentStyle, ModalButton };
--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,16 +1,18 @@
+import * as S from './modal-style';
+
 export const Modal = ({ content, onClose, isOpen }) => {
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div>
-      <div>
-        <p>{content}</p>
-        <button type='button' onClick={onClose}>
+    <S.ModalContainer>
+      <S.ModalContent>
+        <S.ModalContentStyle>{content}</S.ModalContentStyle>
+        <S.ModalButton type='button' onClick={onClose}>
           확인
-        </button>
-      </div>
-    </div>
+        </S.ModalButton>
+      </S.ModalContent>
+    </S.ModalContainer>
   );
 };
