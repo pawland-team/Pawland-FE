b4bf4d4ace3953d5db446c4f92235e8cda5047d1
clyde <clydehan0401@gmail.com>
2024-04-26 03:38:36 +0900

    feat: 모달 기능 구현

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,9 +1,13 @@
-export const Modal = ({ content, onClick }) => {
+export const Modal = ({ content, onClose, isOpen }) => {
+  if (!isOpen) {
+    return null;
+  }
+
   return (
     <div>
       <div>
         <p>{content}</p>
-        <button type='button' onClick={onClick}>
+        <button type='button' onClick={onClose}>
           확인
         </button>
       </div>
