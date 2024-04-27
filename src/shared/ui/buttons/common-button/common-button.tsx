711c7680ff709f2d4452f895d969ee0db69016e3
mingming <jimin23623@gmail.com>
2024-04-27 23:34:39 +0900

    chore: jsdocs 불필요한 타입 설명 삭제

--- src/shared/ui/buttons/common-button/common-button.tsx
+++ src/shared/ui/buttons/common-button/common-button.tsx
@@ -19,14 +19,14 @@ interface CommonButtonProps {
  * - 단위까지 다 적어주세요
  * @param children button 안에 들어갈 요소
  * @param handleClick onClick 이벤트
- * @param {number} borderRadius default 6px (string)
- * @param {number} maxWidth default 100% (string)
- * @param {number} fontSize default 1.6rem (string)
- * @param {string} fontColor default #fff (string)
- * @param {string} padding default '10px 0' (string)
- * @param {number} fontWeight default 400 (string)
- * @param {string} backgroundColor default #000 (string)
- * @param {string} type default 'button'
+ * @param borderRadius default 6px
+ * @param maxWidth default 100%
+ * @param fontSize default 1.6rem
+ * @param fontColor default #fff
+ * @param padding default '10px 0'
+ * @param fontWeight default 400
+ * @param backgroundColor default #000
+ * @param type default 'button'
  */
 
 const CommonButton = ({
