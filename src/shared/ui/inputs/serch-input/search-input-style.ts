335a113187d996f7f6bd22d5b4e5d7e073f53680
mingming <jimin23623@gmail.com>
2024-04-29 15:53:02 +0900

    design: input style 변경

--- src/shared/ui/inputs/serch-input/search-input-style.ts
+++ src/shared/ui/inputs/serch-input/search-input-style.ts
@@ -10,15 +10,12 @@ export const SearchInputBox = styled.div<SearchInputBoxStyle>`
   display: flex;
   justify-content: center;
   width: 100%;
+  max-width: ${(props) => props.$maxWidth};
 
   form {
     ${inputContainer}
-    max-width: ${(props) => props.$maxWidth};
-
     gap: 26px;
-
     padding: 15px 34px;
-
     border-radius: 37px;
     border: 2px solid ${({ theme }) => theme.color.blue_43ADFF};
   }
