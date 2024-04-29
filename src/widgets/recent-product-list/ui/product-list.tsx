8317a8d460300303d551ee3ca38cc188189a2e5c
mingming <jimin23623@gmail.com>
2024-04-29 15:11:46 +0900

    chore: ProductList jsdocs 추가

--- src/widgets/recent-product-list/ui/product-list.tsx
+++ src/widgets/recent-product-list/ui/product-list.tsx
@@ -11,6 +11,9 @@ interface ProductListProps {
 /**
  * 상품 카드 리스트
  * - props 내려주지 않으면 기본 4개 보여지도록 설정되어있음 (메인/상품 리스트 디폴트)
+ * @param cardListFlexGap 카드 리스트 flex gap
+ * @param flexGap 카드 아이템 flex gap
+ * @param cardNumberPerRow 한줄 카드 갯수
  */
 
 const ProductList = ({ cardListFlexGap = '60px 30px', cardItemFlexGap, cardItemNumberPerRow }: ProductListProps) => {
