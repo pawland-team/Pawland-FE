3b396aab703efb5489925f8559137ec54dc9d9b8
clyde <clydehan0401@gmail.com>
2024-04-30 11:24:34 +0900

    design: 페이지 영역 나누기

---
+++ src/pages/community/post/post-page-style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const PostPage = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+export { PostPage };
--- src/pages/community/post/post-page.tsx
+++ src/pages/community/post/post-page.tsx
@@ -1,7 +1,13 @@
+import * as S from './post-page-style';
+
 export const CommunityPostPage = () => {
   return (
-    <div>
-      <h1>커뮤니티 포스트 페이지</h1>
-    </div>
+    <S.PostPage>
+      <h1>헤더 영역</h1>
+      <h1>지역카테고리선택영역</h1>
+      <h1>게시글 제목 입력 영역</h1>
+      <h1>텍스트 에디터 영역</h1>
+      <h1>대표 이미지 업로드 영역</h1>
+    </S.PostPage>
   );
 };
