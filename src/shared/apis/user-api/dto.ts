f425425220e7b16d898cc9214ca4f652a863b8ab
Vegatality <vegatality@gmail.com>
2024-05-01 15:34:08 +0900

    chore: user dto 수정

--- src/shared/apis/user-api/dto.ts
+++ src/shared/apis/user-api/dto.ts
@@ -5,7 +5,7 @@ export interface UserEntity {
    * user Id
    */
   id: number;
-  profileImage: string | null;
+  profileImage: string;
   nickname: string;
   email: string;
   userIntroduce: string;
@@ -17,4 +17,16 @@ export interface UserEntity {
    */
   stars: number;
   loginType: LoginType;
+}
+
+export interface GetUserInfoResponse {
+  id: UserEntity['id'];
+  profileImage: UserEntity['profileImage'];
+  nickname: UserEntity['nickname'];
+  email: UserEntity['email'];
+  /**
+   * TODO: UserEntity에 적힌 userIntroduce랑 필드명이 다름.
+   */
+  userDesc: UserEntity['userIntroduce'];
+  stars: UserEntity['stars'];
 }
