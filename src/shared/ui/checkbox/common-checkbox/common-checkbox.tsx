c36f9655b4ac2a002af0e51682e380bc7a0542de
mingming <jimin23623@gmail.com>
2024-05-01 14:38:22 +0900

    design: checkbox 체크 아이콘 이미지에서 css로 대체

--- src/shared/ui/checkbox/common-checkbox/common-checkbox.tsx
+++ src/shared/ui/checkbox/common-checkbox/common-checkbox.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import Image from 'next/image';
-
 import * as S from './common-checkbox-style';
 
 interface CommonCheckBoxProps {
@@ -26,9 +24,7 @@ const CommonCheckBox = ({ label, group, checked = false }: CommonCheckBoxProps) 
 
   return (
     <S.CommonCheckBoxContainer>
-      <div className={isChecked ? 'checkbox checked' : 'checkbox'}>
-        {isChecked && <Image width={11} height={9} alt='체크되었음' src='/images/icon/checkbox-checked-icon.svg' />}
-      </div>
+      <div className={isChecked ? 'checkbox checked' : 'checkbox'} />
       <label htmlFor={label}>{label}</label>
       <input type='checkbox' name={group} id={label} checked={isChecked} onChange={handleChangeCheckBox} />
     </S.CommonCheckBoxContainer>
