93499fbb0211bb9e07a3bb21b3ad9363dc9ff4a5
mingming <jimin23623@gmail.com>
2024-05-02 19:20:14 +0900

    chore: common select도 외부 클릭시 닫히도록

--- src/shared/ui/select-box/common-select-box/common-select-box.tsx
+++ src/shared/ui/select-box/common-select-box/common-select-box.tsx
@@ -1,6 +1,8 @@
-import { Dispatch, MouseEvent, SetStateAction, useState } from 'react';
+import { Dispatch, MouseEvent, SetStateAction, useRef, useState } from 'react';
 
 import styled from 'styled-components';
+
+import { useOutsideClick } from '@shared/hooks/use-outside-click';
 
 import { DropDownBox, DropDownBoxDto } from '../ui/drop-down-box';
 import { SelectBox } from '../ui/select-box';
@@ -14,11 +16,14 @@ interface CommonSelectBoxProps {
 /**
  *
  * 리스트 클릭했을 때 클릭된 value 값이 보여져야하는 경우 사용.
- * 주로 최신순/인기순 소팅 셀렉트 박스로 활용.
+ * 주로 최신순/인기순 소팅 셀렉트 박스로 활용. (예시는 상품 리스트에서 확인)
  */
 
 const CommonSelectBox = ({ selectedName, dropdownList, setSelectedSortingName }: CommonSelectBoxProps) => {
+  const dropDownRef = useRef(null);
   const [isOpened, setIsOpened] = useState(false);
+
+  useOutsideClick(dropDownRef, isOpened, setIsOpened);
 
   const handleClickOpenSelectBox = () => {
     setIsOpened((prev) => !prev);
@@ -34,7 +39,7 @@ const CommonSelectBox = ({ selectedName, dropdownList, setSelectedSortingName }:
 
   return (
     <>
-      <StyleCommonSelectBox>
+      <StyleCommonSelectBox ref={dropDownRef}>
         <SelectBox handleClick={handleClickOpenSelectBox} selectedName={selectedName} isOpened={isOpened} />
         {isOpened && (
           <DropDownBox>
