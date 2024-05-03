9d5bca4263d8af09ed12ad4f9442cdbafd1e6728
mingming <jimin23623@gmail.com>
2024-05-03 13:45:30 +0900

    chore: 주석 추가

--- src/shared/hooks/use-outside-click/use-outside-click.tsx
+++ src/shared/hooks/use-outside-click/use-outside-click.tsx
@@ -1,5 +1,12 @@
 import { useEffect } from 'react';
 
+/**
+ * 특정 영역의 바깥 클릭시 닫힐 수 있도록 처리
+ * @param ref 클릭해도 안닫히는 영역 (이 영역 외의 부분 클릭시 닫힘)
+ * @param isOpened 닫고 싶은 영역의 열림 상태
+ * @param setIsOpened 닫고 싶은 영역의 setState
+ */
+
 const useOutsideClick = (
   ref: React.RefObject<HTMLElement>,
   isOpened: boolean,
