d6f770639b96c663d9f5bbe365481a9c010a04b9
Vegatality <vegatality@gmail.com>
2024-05-06 05:52:24 +0900

    fix: storybook static image import 경로 수정

--- src/shared/ui/profile/round-profile/round-profile.stories.tsx
+++ src/shared/ui/profile/round-profile/round-profile.stories.tsx
@@ -1,8 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-// import mockbird from '@/public/images/mock/bird-w64-h64.png';
-
 import { RoundProfile } from './round-profile';
 
 const meta = {
