c8de3e3145df514bf71421fcf8a2ab738cd05c8a
mingming <jimin23623@gmail.com>
2024-05-05 22:57:12 +0900

    chore: 파일명 변경

--- src/shared/ui/buttons/index.ts
+++ src/shared/ui/buttons/index.ts
@@ -2,6 +2,6 @@ export { CommonButton } from './common-button/common-button';
 
 export { RoundedArrowButton } from './rounded-arrow-button/rounded-arrow-button';
 
-export { ScrollToButton } from './scroll-to-button/scroll-top-button';
+export { ScrollToButton } from './scroll-to-button/scroll-to-button';
 
 export { RoundedCancelButton } from './rounded-cancel-button/rounded-cancel-button';
