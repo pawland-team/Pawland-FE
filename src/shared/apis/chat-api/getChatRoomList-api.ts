6cfcc832052c09ad615ba153a77d7d8a9253d100
Vegatality <vegatality@gmail.com>
2024-05-05 22:48:07 +0900

    feat: chat api 추가

--- src/shared/apis/chat-api/getChatRoomList-api.ts
+++
@@ -1,8 +0,0 @@
-import { clientWithTokenApi } from '../instance';
-import { ChatRoomResponse } from './dto';
-
-export const getChatRoomList = async () => {
-  const response = await clientWithTokenApi.get<ChatRoomResponse>('/api/chat/rooms');
-
-  return response.data;
-};
