47c3a02ff2e2a4da5dce93424dcede43e42c5c04
Vegatality <vegatality@gmail.com>
2024-05-05 22:41:54 +0900

    feat: roun-profile 스토리 추가
    img 태그가 아니라 Nextjs Image 컴포넌트 사용

---
+++ src/shared/ui/profile/round-profile.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import mockbird from '@/public/images/mock/bird-w64-h64.png';
+
+import { RoundProfile } from './round-profile';
+
+const meta = {
+  title: 'atoms/profile/RoundProfile',
+  component: RoundProfile,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: {
+      description: 'onClick event handler',
+      type: 'function',
+    },
+    alt: {
+      description: '프로필 이미지에 대한 설명',
+      type: 'string',
+    },
+  },
+  args: {
+    onClick: fn(),
+    src: mockbird,
+  },
+} satisfies Meta<typeof RoundProfile>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: '기본 프로필',
+  args: {
+    alt: '새 사진',
+    size: {
+      onDesktop: {
+        width: '64px',
+        height: '64px',
+      },
+    },
+  },
+};
