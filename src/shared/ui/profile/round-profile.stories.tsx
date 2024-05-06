e3e59e1484b99641f47891ba96be3f6c147b57fb
Vegatality <vegatality@gmail.com>
2024-05-06 10:01:44 +0900

    fix: round profile 스토리북 이미지 상대경로로 수정

--- src/shared/ui/profile/round-profile.stories.tsx
+++ src/shared/ui/profile/round-profile.stories.tsx
@@ -1,9 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import mockbird from 'public/images/mock/bird-w64-h64.png';
-
 import { RoundProfile } from './round-profile';
+import mockbird from '../../../../public/images/mock/bird-w64-h64.png';
 
 const meta = {
   title: 'atoms/profile/RoundProfile',
