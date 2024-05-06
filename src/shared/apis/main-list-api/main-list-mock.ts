45144b9aa02bc2348e9502fb2463b5073e5db263
밍밍이 <134386378+mingzzi96@users.noreply.github.com>
2024-05-07 00:02:48 +0900

    design: 상세 페이지 UI 작업 (mock data 사용) + 상품 리스트 수정 (#64)
    * style: storybook 폴더 변경
    
    * design: button disabled 추가
    
    * design: borderbutton style 수정
    
    * design: checkbox 체크 아이콘 이미지에서 css로 대체
    
    * chore: page import export 변경
    
    * chore: product list/ main product list 파일 이동 및 이름 변경
    
    * chore: product card item 수정
    
    * chore: product list -> flex list로 이름 변경 및 mock data추가
    
    * chore: isWished 상태 보여질 수 있도록 설정
    
    * chore: productList -> productFlexList로 변경
    
    * chore: loading 추가
    
    * chore: layout suspense 변경
    
    * chore: product card item
    
    * design: 동그란 화살표 버튼 컴포넌트
    
    * chore: 찜 누르면 페이지 이동 막기
    
    * design: swiperlist disabled처리
    
    * chore: console.log 삭제
    
    * design: button disabled 스타일 변경
    
    * chore: mainbanner또 폴더 이동
    
    * design: style 수정
    
    * design: scorll button 스타일 설정
    
    * chore: 쓰로틀링 커스텀 훅
    
    * chore: select box 수정
    
    * chore: selectbox 컴포넌트 일괄 수정 및 체크박스 셀렉트박스 추가
    
    * design: storybook 수정
    
    * design: checkbox select box 추가
    
    * design : 간격 수정
    
    * chore: dev merge하고 린트 fix
    
    * design: checkbox storybook 수정
    
    * feat: 외부 영역 클릭시 닫힐 수 있도록 하는 커스텀 훅 생성
    
    * chore: common select도 외부 클릭시 닫히도록
    
    * chore: 주석 추가
    
    * design: border checkbox 생성
    
    * chore: scroll 버튼 이름 변경
    
    * chore: 오류 수정
    
    * chore: rounded-arrow-button 파일명 변경
    
    * chore: features로 폴더 이동
    
    * chore: 필요없는 파일 import 삭제
    
    * chore: 폴더 widget으로 다시 이동
    
    * design: 셀렉트 박스 어로우 수정
    
    * chore: style 변경
    
    * design: 디자인 수정
    
    * chore: 주석 추가
    
    * chore: 상품 리스트 zustand로 동작하도록 관리
    
    * chore: store dev tools 추가
    
    * feat: 일단 여기까지
    
    * chore: 주석 수정
    
    * chore: image 컴포넌트 경고 해결
    
    * chore: 주석
    
    * chore: 일단 기능 한다!!!
    
    * chore: checkbox type 오류 수정
    
    * chore: 타입 오류 해결
    
    * chore: initial value대신에 updatedValueList로 관리
    
    * chore: sorting 셀렉트 박스 수정
    
    * chore: 셀렉트 박스 이름 변경
    
    * chore: console 삭제
    
    * design: 스크롤 버튼 사이즈 변경
    
    * chore: 파일명 변경
    
    * chore: 파일명 변경
    
    * feat: 위로 올라가고 내려오는 버튼
    
    * chore: 폴더명 변경
    
    * chore: 파일/폴더 대이동 및 wish Item 수정중
    
    * chore: 찜버튼 너비 높이 추가
    
    * chore: tostify설치
    
    * feat: 현재 링크 기능
    
    * chore: 무료 나눔 함수 수정
    
    * chore: detail info
    
    * design: 썸네일 클릭하면 바뀌도록
    
    * design: 상세 페이지 디자인 추가
    
    * chore: suspense 안먹히 ㅁ삭제
    
    * chore: layout 로그인/회원가입 페이지 빠진거 되돌리ㅣㄱ
    
    * chore: 헤더 드롭다운 외부 영역 클릭시 닫히도록
    
    * chore: 셀렉트 박스 라벨 수정
    
    * feat: 상품 상세 api 및 mock 데이터 추가
    
    * chore: pull받고 나니 설치하라고 에러 떠서 설치했는데 뭐 변경된건 없는 매직
    
    * design: active border 너무 두꺼운 거 같아서 수정
    
    * feat: mock data 상품 상세에 적용
    
    * chore: 상품 상세
    
    * design: content도 넣기
    
    * design: content 수정
    
    * chore: package.json 실수로 덮어씌워진거 수정
    
    * design: 평점 컴포넌트 가져와서 넣기
    
    * chore: 불필요한 파일 삭제
    
    * feat: 필터 박스 초기화 버튼 추가
    
    * chore: 고유 key값 item과 index 섞어서 만들어줌
    
    * Chore: lint 수정
    
    * chore: wishList props 수정
    
    * chore: storybook 수정
    
    * chore: image import 말고 string으로 변경

---
+++ src/shared/apis/main-list-api/main-list-mock.ts
@@ -0,0 +1,76 @@
+import { mainProductInfo } from './dto';
+
+export const mainListData: mainProductInfo[] = [
+  {
+    id: 0,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 1,
+    category: 'food',
+    price: 0, // 상품 가격
+    productName: '울 강아지가 싫어하는 장난감 어차피 안써서 무료나눔해요. 직거래만 해요.', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: true, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 2,
+    category: 'food',
+    price: 0, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: true, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 3,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 4,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 5,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 6,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+  {
+    id: 7,
+    category: 'food',
+    price: 20000, // 상품 가격
+    productName: '강아지가 좋아하는 대형견용 사료 어떠신지?', // 상품 이름
+    views: 20,
+    imageThumbnail: '/images/mock/product-card-test-image.png', // 대표 이미지
+    isWished: false, // 내가 찜 했는지 안했는지
+  },
+];
