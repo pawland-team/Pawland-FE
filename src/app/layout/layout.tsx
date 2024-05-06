58fc32fe911e1e2c4ba5496580377ad3851611ad
mingming <jimin23623@gmail.com>
2024-05-06 15:14:15 +0900

    chore: layout 로그인/회원가입 페이지 빠진거 되돌리ㅣㄱ

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -34,6 +34,20 @@ const Layout = ({ children }: LayoutProps) => {
       </>
     );
   }
+
+  return (
+    <S.LayoutPage>
+      <main>{children}</main>
+      <S.StyledToastContainer
+        position='bottom-center' // 알람 위치 지정
+        autoClose={4000} // 자동 off 시간
+        hideProgressBar={false} // 진행시간바 숨김
+        closeOnClick // 클릭으로 알람 닫기
+        rtl={false} // 알림 좌우 반전
+        theme='light'
+      />
+    </S.LayoutPage>
+  );
 };
 
 export { Layout };
