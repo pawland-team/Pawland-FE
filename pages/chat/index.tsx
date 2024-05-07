0b2d2738cb06cdb1fc8afca3fabba994819c715c
Vegatality <vegatality@gmail.com>
2024-05-07 11:23:11 +0900

    feat: auth hoc 생성

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,3 +1,14 @@
+import { withAuth } from '@app/hoc/withAuth';
 import { ChatPage } from '@pages/chat';
 
-export default ChatPage;
+export default withAuth(ChatPage);
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// export const getServerSideProps: GetServerSideProps<{ a: string }> = withGetServerSideProps(async (context) => {
+//   return {
+//     props: {
+//       a: 'a',
+//     },
+//   };
+// });
