54e40ce568151ecee6174e27f9f51f97d2ecc99f
mingming <jimin23623@gmail.com>
2024-05-09 01:57:20 +0900

    chore: 상품 리스트 위젯 주석 추가

--- src/widgets/product-search-result-list/product-search-result-list.tsx
+++ src/widgets/product-search-result-list/product-search-result-list.tsx
@@ -2,6 +2,10 @@ import { Loading } from '@app/layout/loading';
 import { useGetSearchResultList } from '@entities/product/hooks/use-get-search-result-list.query';
 import { ProductFlexList } from '@entities/product/ui';
 
+/**
+ * 상품 리스트 (검색 결과) + 페이지네이션 같이 있는 컴포넌트
+ */
+
 const ProductSearchResultList = () => {
   const initialParams = {
     page: 1,
