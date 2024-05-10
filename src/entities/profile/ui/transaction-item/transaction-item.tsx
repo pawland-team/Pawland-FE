8ecb8b2ef3ddbd919f59a198a7ed2fb04e1230c1
jinyoung1018 <71465981+jinyoung1018@users.noreply.github.com>
2024-05-10 11:50:32 +0900

    Feat: 프로필페이지 커뮤니티 리스트 api 연동, 유저페이지 ui, 폴더구조 변경
    * chore: 파일명 변경
    
    * feat: 헤더 프로필 페이지 연동
    
    * feat: 유저 프로필 영역, 등록상품 영역
    
    * feat: 유저 등록상품 리스트 수정, 받은 리뷰 리스트
    
    * feat: 유저페이지 커뮤니티 리스트
    
    * chore: 유저페이지 유저 작성 리뷰 부분 제거
    
    * refactor: 파일명 및 파일구조 변경
    
    * chore: 린트 에러 수정
    
    * chore: 린트 오류 수정
    
    * chore: 린트 오류 수정
    
    * chore: conflict해결
    
    * chore: 린트 에러 수정

--- src/entities/profile/ui/transaction-item/transaction-item.tsx
+++ src/entities/profile/ui/transaction-item/transaction-item.tsx
@@ -1,7 +1,15 @@
+<<<<<<<< HEAD:src/entities/profile/ui/transaction-item/transaction-item.tsx
+========
+import { ReviewItem } from '@entities/profile/review-item/ui';
+>>>>>>>> 7cc1cd6 (Feat: 프로필페이지 커뮤니티 리스트 api 연동, 유저페이지 ui, 폴더구조 변경 ):src/entities/profile/transaction-item/ui/transaction-item.tsx
 import { GoDetailButton } from '@features/button/go-detail-button';
 import { SmallThumbnail } from '@shared/ui/thumbnails/small-thumbnail';
 import { formatPriceToKoStyle } from '@shared/utils/price';
 import { formatDateShorter } from '@shared/utils/time';
+<<<<<<<< HEAD:src/entities/profile/ui/transaction-item/transaction-item.tsx
+========
+import { ReviewWritingArea } from '@widgets/profile-page-review-writing-area';
+>>>>>>>> 7cc1cd6 (Feat: 프로필페이지 커뮤니티 리스트 api 연동, 유저페이지 ui, 폴더구조 변경 ):src/entities/profile/transaction-item/ui/transaction-item.tsx
 
 import * as S from './transaction-item-style';
 
