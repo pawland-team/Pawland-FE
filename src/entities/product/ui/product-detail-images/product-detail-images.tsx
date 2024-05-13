ad7c470b296c0c53e2a33bd2becdcc6e9080f37b
mingming <jimin23623@gmail.com>
2024-05-13 11:19:57 +0900

    chore: subImageList 배열 있고 없고 분기처리

--- src/entities/product/ui/product-detail-images/product-detail-images.tsx
+++ src/entities/product/ui/product-detail-images/product-detail-images.tsx
@@ -7,27 +7,35 @@ import { SubImagesSlide } from '@shared/ui/product';
 import * as S from './product-detail-images-style';
 
 interface ProductDetailImagesProps {
-  ThumbnailSrc: string;
-  SubImagesList: string[];
+  thumbnailSrc: string;
+  subImagesList: string[];
 }
 
-const ProductDetailImages = ({ ThumbnailSrc, SubImagesList }: ProductDetailImagesProps) => {
-  // 썸네일 이미지를 subImageList 맨 앞에 넣어줌
-  const modifiedImageList = [ThumbnailSrc, ...SubImagesList];
-  // 선택된 Image를 저장한다.
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+const ProductDetailImages = ({ thumbnailSrc, subImagesList }: ProductDetailImagesProps) => {
+  if (subImagesList !== undefined && subImagesList.length > 0) {
+    // 썸네일 이미지를 subImageList 맨 앞에 넣어줌
+    const modifiedImageList = [thumbnailSrc, ...subImagesList];
+    // 선택된 Image를 저장한다.
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
-  return (
-    <S.ProductDetailImagesArea>
-      {SubImagesList.length > 0 && (
+    return (
+      <S.ProductDetailImagesArea>
         <SubImagesSlide
           imagesList={modifiedImageList}
           setSelectedIndex={setSelectedImageIndex}
           selectedIndex={selectedImageIndex}
         />
-      )}
+        <S.DetailThumbnailBox>
+          <Image src={modifiedImageList[selectedImageIndex]} fill sizes='582px' alt='상품 대표 이미지' />
+        </S.DetailThumbnailBox>
+      </S.ProductDetailImagesArea>
+    );
+  }
+
+  return (
+    <S.ProductDetailImagesArea>
       <S.DetailThumbnailBox>
-        <Image src={modifiedImageList[selectedImageIndex]} fill sizes='582px' alt='상품 대표 이미지' />
+        <Image src={thumbnailSrc} fill sizes='582px' alt='상품 대표 이미지' />
       </S.DetailThumbnailBox>
     </S.ProductDetailImagesArea>
   );
