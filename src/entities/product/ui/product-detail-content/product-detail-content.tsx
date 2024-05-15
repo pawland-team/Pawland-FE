41d24944f17973a3ff4b04477c030857190bc715
mingming <jimin23623@gmail.com>
2024-05-15 20:26:25 +0900

    feat: 제품 상세 썸네일 나오도록

--- src/entities/product/ui/product-detail-content/product-detail-content.tsx
+++ src/entities/product/ui/product-detail-content/product-detail-content.tsx
@@ -18,13 +18,19 @@ const ProductDetailContent = ({ detailContent }: ProductDetailContentProps) => {
     setIsOpened((prev) => !prev);
   };
 
-  useEffect(() => {
-    const textAreraHeight = contentTextRef.current?.offsetHeight;
+  // ? : 화면 렌더링 되기 전에 먼저 요소 높이를 측정해 버리기 때문에 항상 500px이 넘지 않는 상황.
+  // setTimeout으로 해결 이게 맞나
 
-    if (textAreraHeight) {
-      setTextBoxHeight(contentTextRef.current.offsetHeight);
-    }
-  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (contentTextRef.current) {
+        const height = contentTextRef.current.offsetHeight;
+        setTextBoxHeight(height);
+      }
+    }, 100); // 1초 후에 실행
+
+    return () => clearTimeout(timer); // cleanup 함수에서 타이머 해제
+  }, []); // []를 사용하여 최초 렌더링 시에만 실행되도록 설정
 
   return (
     <SContentContainer>
