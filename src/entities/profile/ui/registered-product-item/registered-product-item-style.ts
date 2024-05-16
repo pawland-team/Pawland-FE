5ab0835414b045837aa84e9390623342cf0e39c0
jinyoung1018 <71465981+jinyoung1018@users.noreply.github.com>
2024-05-16 18:43:31 +0900

    Feat:  프로필 수정기능 수정
    * feat: react-toastify 설치
    
    * feat: 로그인 정보 데이터 연동
    
    * chore: console.log 제거
    
    * chore: 파일명 및 파일 구조 변경, 프로필 dto추가,api추가
    
    * feat: 프로필페이지 찜한상품 api연동
    
    * feat: 프로필페이지 등록상품 api연동
    
    * feat: 유저페이지 유저정보 api 연동
    
    * chore: 타입수정
    
    * chore: 린트 에러 수정
    
    * chore: 린트 에러 수정
    
    * feat: 유저페이지 등록상품 api연동
    
    * feat: 유저 게시글 조회 api 연동
    
    * feat: 유저 리뷰 조회 api 연동
    
    * feat: 거래내역 api, dto 추가
    
    * feat: 판매상품 api 연동
    
    * feat: 거래내역 리뷰쓰기
    
    * feat: 유저 커뮤니티 페이지네이션
    
    * feat: 내커뮤니티 페이지네이션
    
    * chore: dto수정, 페이지네이션
    
    * feat: 프로필 페이지 기능
    
    * chore: 린트 에러 수정
    
    * 린트 에러 수정
    
    * 린트 에러 수정
    
    * 린트 에러 수정
    
    * 린트 에러 수정
    
    * chore: 내등록상품 판매상태ui 수정
    
    * chore: 프로필 수정 기능 수정
    
    * chore: 컨플릭트 해결

--- src/entities/profile/ui/registered-product-item/registered-product-item-style.ts
+++ src/entities/profile/ui/registered-product-item/registered-product-item-style.ts
@@ -14,45 +14,51 @@ export const RegisteredProductItem = styled.div`
   border: 1px solid ${({ theme }) => theme.color.gray_F3F3F3};
   border-radius: 6px;
 
-  position: relative;
-
   &:hover {
     box-shadow: 4px 4px 4px 0 rgb(0 0 0 / 5%);
   }
 `;
 
 export const DropDownArea = styled.div`
-  margin-left: 27px;
-  margin-top: 14px;
   display: flex;
   flex-direction: column;
   gap: 14px;
+
+  margin-top: 14px;
+  margin-left: 27px;
 `;
+
 export const IconPriceArea = styled.div`
   /* margin-left: 200px; */
   position: absolute;
   right: 26px;
-  margin-top: 14px;
+
   display: flex;
   flex-direction: column;
   gap: 14px;
+
+  margin-top: 14px;
 `;
+
 export const EditIcon = styled(Image)`
   float: right;
 `;
 
 export const ProductName = styled.h1`
-  font-size: 2.4rem;
-  margin-left: 14px;
-  margin-top: 14px;
   width: 100px;
+  margin-top: 14px;
+  margin-left: 14px;
+
+  font-size: 2.4rem;
   white-space: nowrap;
 `;
+
 export const Date = styled.span`
   font-size: 1.6rem;
   line-height: 28px;
   color: ${({ theme }) => theme.color.gray_9E9E9E};
 `;
+
 export const Price = styled.p`
   font-size: 3.2rem;
   font-weight: 700;
@@ -60,6 +66,13 @@ export const Price = styled.p`
 `;
 
 export const Status = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 80px;
+  height: 30px;
+
   font-size: 1.5rem;
   font-weight: 600;
   color: black;
@@ -67,9 +80,4 @@ export const Status = styled.div`
   border: 1px solid;
   border-color: #9e9e9e;
   border-radius: 5px;
-  width: 80px;
-  height: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
