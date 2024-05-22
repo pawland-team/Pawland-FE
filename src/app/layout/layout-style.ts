a239689b4abee2007948d0147d9e975b176e1963
mingming <jimin23623@gmail.com>
2024-05-22 21:02:46 +0900

    chore: 로딩시 페이지 전체 깜빡임 현상 제거 (내용이 없어서 푸터가 끌어올려지는 현상 제거)

--- src/app/layout/layout-style.ts
+++ src/app/layout/layout-style.ts
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 
 export const LayoutPage = styled.div`
   padding-top: 80px;
+
+  main {
+    min-height: calc(100vh - 80px);
+  }
 `;
 
 export const StyledToastContainer = styled(ToastContainer)`
