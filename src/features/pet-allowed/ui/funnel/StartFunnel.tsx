8a03bc100f2a467a006d1022feb3dab8d3740e62
mingming <jimin23623@gmail.com>
2024-06-17 13:26:08 +0900

    design: start page 레이아웃 완성

--- src/features/pet-allowed/ui/funnel/StartFunnel.tsx
+++ src/features/pet-allowed/ui/funnel/StartFunnel.tsx
@@ -1,7 +1,23 @@
+import { CommonButton } from '@shared/ui/buttons';
+
 import * as S from './style';
 
 const StartFunnel = () => {
-  return <S.FunnelWrap>wrapper</S.FunnelWrap>;
+  return (
+    <S.FunnelArea>
+      <div className='funnel-container'>
+        <div className='funnel-title-box'>
+          <h2>반려동물 동반 장소 검색 서비스</h2>
+          <p>함께 방문할 수 있는 장소를 쉽게 검색해 보세요!</p>
+        </div>
+        <div className='funnel-button-box'>
+          <CommonButton fontSize='1.8rem' fontWeight='600' fontColor='#fff' backgroundColor='#43ADFF' padding='23px 0'>
+            시작하기
+          </CommonButton>
+        </div>
+      </div>
+    </S.FunnelArea>
+  );
 };
 
 export { StartFunnel };
--- src/features/pet-allowed/ui/funnel/style.ts
+++ src/features/pet-allowed/ui/funnel/style.ts
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
 
-export const FunnelWrap = styled.section`
-  height: 100%;
+export const FunnelArea = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  min-height: calc(100vh - ${({ theme }) => theme.header.web});
+
+  .funnel-container {
+    width: 100%;
+    max-width: 980px;
+    text-align: center;
+  }
+
+  .funnel-title-box {
+    margin-bottom: 40px;
+
+    h2 {
+      margin-bottom: 24px;
+      font-size: 32px;
+      font-weight: 600;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+
+  .funnel-button-box {
+    max-width: 600px;
+    margin: 0 auto;
+  }
 `;
